efe45af004739bb419c5b3a4a3727a15d3f5cad0
kareem roqib <kareemolakunleh@gmail.com>
2023-05-02 10:00:45 +0100

    run prettier and eslint script

--- js/index.js
+++ js/index.js
@@ -14,9 +14,9 @@ const bookList = document.querySelector('.added-books');
 const addToLocalStorage = data => {
   localStorage.setItem('book', JSON.stringify(data));
 };
-const getLocalStorage = () => {
-  return (localStorage.getItem('book') ? JSON.parse(localStorage.getItem('book')) : [])
-};
+function getLocalStorage() {
+  return localStorage.getItem('book') ? JSON.parse(localStorage.getItem('book')) : [];
+}
 const removeLocalStorage = id => {
   const books = getLocalStorage();
   return books.filter(book => book.id !== id);
