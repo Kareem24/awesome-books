487abde963c602f15b3945dd3ae6be52f85acd78
kareem roqib <kareemolakunleh@gmail.com>
2023-07-07 23:27:37 +0100

    add neccessary styles so as to forma the correct project layout structure

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ class GetElement {
   constructor(selector, isList) {
     this.isList = isList;
     this.selector = selector;
-    if (this.isList) return document.querySelectorAll(this.selector);
-
-    return document.querySelector(this.selector);
+    return this.isList ? document.querySelectorAll(this.selector)
+      : document.querySelector(this.selector);
   }
 }
 class LocalStorage {
@@ -12,11 +11,11 @@ class LocalStorage {
     this.name = name;
   }
 
-  add = data => localStorage.setItem(this.name, JSON.stringify(data));
+  addToLocalStorage = data => localStorage.setItem(this.name, JSON.stringify(data));
 
-  get = () => (localStorage.getItem(this.name) ? JSON.parse(localStorage.getItem(this.name)) : []);
+  getFromLocalStorage = () => (localStorage.getItem(this.name) ? JSON.parse(localStorage.getItem(this.name)) : []);
 
-  remove = id => this.get().filter(book => book.id !== id);
+  removeFromLocalStorage = id => this.getFromLocalStorage().filter(book => book.id !== id);
 }
 class Book {
   constructor() {
@@ -25,41 +24,48 @@ class Book {
     this.bookList = new GetElement('.added-books', false);
   }
 
-  #delete = e => {
-    const el = e.currentTarget.parentElement;
+  #deleteBook = e => {
+    const el = e.currentTarget.parentElement.parentElement;
     const { id } = el.dataset;
     this.bookList.removeChild(el);
-    this.bookData = this.localStorage.remove(id);
-    this.localStorage.add(this.bookData);
+    this.bookData = this.localStorage.removeFromLocalStorage(id);
+    this.localStorage.addToLocalStorage(this.bookData);
+    if (this.bookList.children.length === 0) {
+      this.bookList.innerHTML = '<p class="red">this book is empty</p>';
+    }
   };
 
-  #create = ({ id, title, author }) => {
+  #createBook = ({ id, title, author }) => {
     const element = document.createElement('article');
     element.classList.add('book');
     const attr = document.createAttribute('data-id');
     attr.value = id;
     element.setAttributeNode(attr);
-    element.innerHTML = `<p class="title">${title}</p>
-        <p class="author">${author} </p>
-        <button type="button" class='delete-btn'>Remove</button>
-        <div class="horizontal-line"></div>`;
+    element.innerHTML = `<div class='details d-flex section-center'>
+    <p class="title">"<span class='book-title'>${title}</span>" by <span class='author'> ${author}</span> </p>
+    <button type="button" class='delete-btn'>Remove</button>
+    </div>
+       `;
 
     this.bookList.appendChild(element);
     const deleteBtn = element.querySelector('.delete-btn');
-    deleteBtn.addEventListener('click', this.#delete);
+    deleteBtn.addEventListener('click', this.#deleteBook);
   };
 
-  add = e => {
+  addToBooklist = e => {
     e.preventDefault();
     const book = new FormData(e.currentTarget);
     const id = new Date().getTime().toString();
     const value = [...book.values()];
     const entries = Object.fromEntries([...book.entries()]);
     if ((value[0].trim() && value[1].trim()).length === 0 || value.includes('')) return;
-    this.bookData = this.localStorage.get();
+    this.bookData = this.localStorage.getFromLocalStorage();
     this.bookData.push({ id, ...entries });
-    this.#create({ id, ...entries });
-    this.localStorage.add(this.bookData);
+    if (this.bookList.textContent === 'this book is empty') {
+      this.bookList.textContent = '';
+    }
+    this.#createBook({ id, ...entries });
+    this.localStorage.addToLocalStorage(this.bookData);
     const title = new GetElement('#title', false);
     const author = new GetElement('#author', false);
     title.value = '';
@@ -67,14 +73,19 @@ class Book {
   };
 
   loadBook = () => {
-    const books = this.localStorage.get();
+    const books = this.localStorage.getFromLocalStorage();
+    const bookList = new GetElement('.added-books');
+
     if (books.length > 0) {
-      books.map(book => this.#create(book));
+      books.map(book => this.#createBook(book));
+    }
+    if (bookList.children.length === 0) {
+      bookList.innerHTML = '<p class="red">this book is empty</p>';
     }
   };
 }
 
 const bookLists = new Book();
 const form = new GetElement('#form', false);
-form.addEventListener('submit', bookLists.add);
+form.addEventListener('submit', bookLists.addToBooklist);
 window.addEventListener('DOMContentLoaded', bookLists.loadBook);
